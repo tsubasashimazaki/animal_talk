1bde4b7af8eeb9fab50ef679bd8191e8ac247301
github tsubasashimazaki <github>
2020-06-04 17:10:18 +0900

    modified: uiとしてVueのインストール new file: fontawesomeの読み込み

---
+++ resources/views/sample.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Title</title>
+</head>
+<body>
+<h1>Vueのテストです。</h1>
+<!-- Vue表示用 -->
+<div id="app">
+    <example-component></example-component>
+</div>
+
+<script src="{{ asset('/js/app.js') }}"></script>
+</body>
+</html>
